0ae0aa78812d18d55490860c8607780999d47085
Ammon Jiaqi Deng <81936138+AmmonDeng@users.noreply.github.com>
2023-10-12 14:49:18 +0200

    Update article.ts

--- WebNews/src/app/interfaces/article.ts
+++ WebNews/src/app/interfaces/article.ts
@@ -1,13 +1,16 @@
-export interface Article{
-    id:number,
-    id_user:number,
-    abstract:string,
-    subtitle:string
-    update_date:string,
-    category:string,
-    title:string,
-    image_data:string,
-    image_media_type:string,
-    body:string,
-    username:string,
+export interface Article {
+  id: number;
+  id_user: number;
+  abstract: string;
+  subtitle: string;
+  update_date: string;
+  category: string;
+  title: string;
+  image_data: string;
+  // service response data is  thumbnail_image and thumbnail_media_type,is not image_data and image_media_type, so add
+  thumbnail_image?: string;
+  image_media_type: string;
+  thumbnail_media_type?: string;
+  body: string;
+  username: string;
 }
