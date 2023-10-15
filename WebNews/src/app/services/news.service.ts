d7ad5aaf35322e66b443872fd33ef34edff45a49
Philipp Walter <p.walter-lauffen@t-online.de>
2023-10-15 14:21:25 +0200

    Fix for logout

--- WebNews/src/app/services/news.service.ts
+++ WebNews/src/app/services/news.service.ts
@@ -57,9 +57,8 @@ export class NewsService {
     return this.http.get<Article[]>(this.newsUrl, this.httpOptions);
   }
 
-  deleteArticle(article: Article | number): Observable<Article> {
-    const id = typeof article === 'number' ? article : article.id;
-    const url = `${this.articleUrl}/${id}`;
+  deleteArticle(article_id: number): Observable<Article> {
+    const url = `${this.articleUrl}/${article_id}`;
     return this.http.delete<Article>(url, this.httpOptions);
   }
 
